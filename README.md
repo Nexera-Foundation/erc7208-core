fdd4baa18a781cf39b88d6a50122e73dde116d8f
Pavel Rubin <pash7ka@gmail.com>
2025-09-18 12:43:43 +0200

    Initialize repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# ERC-7208 interfaces & base contracts
+This repository contains base implementation of ERC-7208 by Nexera Foundation.
---
+++ hardhat.config.ts
@@ -0,0 +1,44 @@
+import type { HardhatUserConfig } from "hardhat/config";
+
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+//import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+
+import { configVariable } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  plugins: [/*hardhatToolboxMochaEthersPlugin, */hardhatToolboxViemPlugin],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.30",
+      },
+      production: {
+        version: "0.8.30",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2000000,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+  },
+};
+
+export default config;
