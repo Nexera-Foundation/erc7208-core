f54df0853e6fa00b397af51ade8d5c51f4105349
Pavel Rubin <pash7ka@gmail.com>
2026-02-18 00:01:20 +0100

    simplify DataPoints usage

--- contracts/utils/DataPoints.sol
+++ contracts/utils/DataPoints.sol
@@ -6,6 +6,9 @@ import {ChainidTools} from "./ChainidTools.sol";
 /// @dev DataPoint is a 32 bytes structure which contains information about data point
 type DataPoint is bytes32;
 
+using DataPoints for DataPoint global;
+using {equals as ==} for DataPoint global;
+
 /**
  * DataPoint structure:
  * 0xPPPPVVRRIIIIIIIIHHHHHHHHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
@@ -78,4 +81,8 @@ library DataPoints {
         chainid = uint32(dpu >> CHAINID_BIT_OFFSET);
         id = uint32(dpu >> ID_BIT_OFFSET);
     }
+}
+
+function equals(DataPoint dp1, DataPoint dp2) pure returns (bool) {
+    return DataPoint.unwrap(dp1) == DataPoint.unwrap(dp2);
 }
