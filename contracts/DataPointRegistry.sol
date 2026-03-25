a971523bb41843afccf53c7088cb0d9baf637905
Pavel Rubin <pash7ka@gmail.com>
2026-03-25 14:42:14 +0100

    fix pragma to have same solidity version everywhere

--- contracts/DataPointRegistry.sol
+++ contracts/DataPointRegistry.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.28;
 
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import {DataPoints, DataPoint} from "./utils/DataPoints.sol";
--- contracts/examples/race-to-the-target-game/RaceToTheTargetDataManager.sol
+++ contracts/examples/race-to-the-target-game/RaceToTheTargetDataManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.28;
 
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
